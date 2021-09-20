e7ca824ac943b5dbd1ba00ad606b1fb98317ea97
Ilya Ivanov <static.ila@gmail.com>
2021-09-21 02:09:18 +0300

    Add engine

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { AnimatedNumber, engine } from "./animationEngine";
 import { root } from "./store";
 
 const createFullScreenCanvas = (): HTMLCanvasElement => {
@@ -23,52 +24,79 @@ document.body.appendChild(canvas);
 const fontSize = 16;
 const color2 = "#2A3135";
 
-const drawCircle = (x: number, y: number, r: number) => {
-  ctx.beginPath();
-  ctx.arc(x, y, r, 0, 2 * Math.PI);
-  ctx.fill();
-};
-
-const drawItem = (x: number, y: number, title: string) => {
-  drawCircle(x, y, 3.5);
-  const xText = x + 10;
-
-  ctx.fillText(title, xText, y + fontSize * 0.3);
-};
-
 const xBase = 40;
+const yBase = 40;
 const xStep = 25;
+const yStep = 25;
+
+type ItemViewModel = {
+  xOffset: number;
+  yOffset: number;
+  item: Item;
+};
+const visibleItems: ItemViewModel[] = [];
+
+let currentYOffset = yBase;
+
+const viewItem = (item: Item, level: number) => {
+  visibleItems.push({
+    item,
+    yOffset: currentYOffset,
+    xOffset: xBase + level * xStep,
+  });
+  currentYOffset += yStep;
+  item.children.forEach((c) => viewItem(c, level + 1));
+};
+
+root.children.forEach((child) => viewItem(child, 0));
 
 const render = () => {
+  const drawCircle = (x: number, y: number, r: number) => {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.fill();
+  };
+
+  const drawItem = (itemView: ItemViewModel) => {
+    const {
+      xOffset,
+      yOffset,
+      item: { title },
+    } = itemView;
+    drawCircle(xOffset, yOffset, 3.5);
+    const xText = xOffset + 10;
+
+    ctx.fillText(title, xText, yOffset + fontSize * 0.3);
+  };
+
   ctx.clearRect(0, 0, Number.MAX_VALUE, Number.MAX_VALUE);
   ctx.fillStyle = color2;
   ctx.font = `${fontSize}px Segoe UI`;
-  let currentXOffset = 0;
 
-  const viewItem = (item: Item, level: number) => {
-    drawItem(xBase + level * xStep, xBase + currentXOffset * xStep, item.title);
-    currentXOffset += 1;
-    item.children.forEach((c) => viewItem(c, level + 1));
-  };
+  visibleItems.forEach(drawItem);
 
-  root.children.forEach((child) => viewItem(child, 0));
-
-  ctx.fillStyle = "rgba(0,0,0,0.1)";
-  ctx.fillRect(0, 40 - 13 + position * xStep, 400000, xStep);
+  drawSelectionBox();
 };
 
-let position = 0;
+const drawSelectionBox = () => {
+  ctx.fillStyle = "rgba(0,0,0,0.1)";
+  ctx.fillRect(0, yBase - yStep / 2 + positionOffset.value, 400000, xStep);
+};
+
+let positionOffset = new AnimatedNumber();
 
 document.addEventListener("keydown", (e) => {
   if (e.code === "ArrowDown") {
-    position += 1;
+    positionOffset.animateToDelta(positionOffset.targetValue + xStep);
     render();
   }
 
   if (e.code === "ArrowUp") {
-    position -= 1;
+    positionOffset.animateToDelta(positionOffset.targetValue - xStep);
     render();
   }
 });
+
+engine.onRender = render;
 
 render();
