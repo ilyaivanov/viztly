704c2a1d83295c31a00bb62486230e519c98134c
Ilya Ivanov <static.ila@gmail.com>
2021-10-02 13:01:51 +0300

    Remove unused design constants

--- src/designSystem/index.ts
+++ src/designSystem/index.ts
@@ -1,25 +1,3 @@
-export const spacings1 = {
-  distanceBetweenTextAndCircle: 5,
-  circleSize: 6,
-  borderWidth: 2,
-  xStep: 20,
-  scrollbar: 8,
-  rootHorizontalPadding: 10,
-  rootVerticalPadding: 10,
-  maxWidth: 700,
-  itemPadding: 3,
-};
-
-export const colors = {
-  appBackground: "#1e1e1e",
-  text: "white",
-  circle: "white",
-  selectionBackground: "#27292A",
-  childrenBorder: "#4C5155",
-  childrenBorderHighlighted: "#808080",
-  scrollbar: "#424242",
-};
-
 export const fontSizes = {
   regular: 13,
   big: 16,
