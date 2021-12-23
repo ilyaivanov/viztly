ca3a50b995319e2e626a3f33992c126e4e2a876b
Ilya Ivanov <static.ila@gmail.com>
2021-12-23 17:22:06 +0200

    Refactor a bit tests and view

--- src/specs/renaming.spec.ts
+++ src/specs/renaming.spec.ts
@@ -4,52 +4,50 @@ import { AppContent, init } from "../app";
 import { sp } from "../view/design";
 import simulation from "./simulation";
 
-describe("Renaming item", () => {
-  describe(`when pressing 'e'`, () => {
-    let app: AppContent;
-    beforeEach(() => {
-      app = init(createRoot([createItem("Item 1"), createItem("Item 2")]));
-      simulation.startRename(app);
+describe(`When pressing 'e' while focusing on Item 1`, () => {
+  let app: AppContent;
+  beforeEach(() => {
+    app = init(createRoot([createItem("Item 1"), createItem("Item 2")]));
+    simulation.startRename(app);
+  });
+
+  it("an input should be rendered at text coordinates", () => {
+    check.input({
+      left: 50 + sp.circleToTextDistance,
+      top: 50 - sp.fontSize * 0.32 * 2.5,
+      text: "Item 1",
     });
+  });
 
-    it("an input should be rendered at text coordinates", () => {
-      check.input({
-        left: 50 + sp.circleToTextDistance,
-        top: 50 - sp.fontSize * 0.32 * 2.5,
-        text: "Item 1",
-      });
-    });
+  it("entering a new text and pressing enter removes input and assigns title to an item", () => {
+    simulation.setValueToInput("New Title");
 
-    it("entering a new text and pressing enter removes input and assigns title to an item", () => {
-      simulation.setValueToInput("New Title");
+    simulation.hitEnter(app);
 
-      simulation.hitEnter(app);
+    check.itemExistsAt(app.views, 1, 1, "New Title");
+    check.itemSelectedHasTitle(app, "New Title");
 
-      check.itemExistsAt(app.views, 1, 1, "New Title");
-      check.itemSelectedHasTitle(app, "New Title");
+    check.inputRemoved();
+  });
 
-      check.inputRemoved();
-    });
+  it("bluring on text input removes it from the DOM", () => {
+    simulation.blurOnInput();
 
-    it("bluring on text input removes it from the DOM", () => {
-      simulation.blurOnInput();
+    check.itemExistsAt(app.views, 1, 1, "Item 1");
+    check.itemSelectedHasTitle(app, "Item 1");
 
-      check.itemExistsAt(app.views, 1, 1, "Item 1");
-      check.itemSelectedHasTitle(app, "Item 1");
+    check.inputRemoved();
+  });
 
-      check.inputRemoved();
-    });
+  it("pressing down or up should not change selection", () => {
+    simulation.pressDown(app);
+    check.itemSelectedHasTitle(app, "Item 1");
 
-    it("pressing down or up should not change selection", () => {
-      simulation.pressDown(app);
-      check.itemSelectedHasTitle(app, "Item 1");
+    simulation.pressUp(app);
+    check.itemSelectedHasTitle(app, "Item 1");
 
-      simulation.pressUp(app);
-      check.itemSelectedHasTitle(app, "Item 1");
-
-      simulation.pressLeft(app);
-      simulation.pressLeft(app);
-      check.itemSelectedHasTitle(app, "Item 1");
-    });
+    simulation.pressLeft(app);
+    simulation.pressLeft(app);
+    check.itemSelectedHasTitle(app, "Item 1");
   });
 });
--- src/view/index.ts
+++
@@ -1,1 +0,0 @@
-export { forEachView, renderViews } from "./views";
