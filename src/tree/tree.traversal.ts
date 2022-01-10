1f3e125bd67ad701c7f59165b42ad68c7c904374
Ilya Ivanov <static.ila@gmail.com>
2022-01-10 08:08:09 +0200

    Refactor tree.traversal a bit

--- src/tree/tree.traversal.ts
+++ src/tree/tree.traversal.ts
@@ -16,14 +16,9 @@ export const getItemBelow = (item: Item): Item | undefined =>
 export const getNextSiblingOrItemBelow = (item: Item): Item | undefined =>
   getFollowingSibling(item) || getFollowingItem(item);
 
-export const getPreviousSiblingOrItemAbove = (item: Item): Item | undefined => {
-  const items = item.parent?.children;
-  if (items) {
-    const index = items.indexOf(item);
-    if (index > 0) return items[index - 1];
-    else return getItemAbove(item);
-  }
-};
+export const getPreviousSiblingOrItemAbove = (item: Item): Item | undefined =>
+  getRelativeSibling(item, (currentIndex) => currentIndex - 1) ||
+  getItemAbove(item);
 
 export const forEachOpenChild = (item: Item, cb: F1<Item>) => {
   const traverse = (children: Item[]) => {
@@ -41,6 +36,7 @@ export const needsToBeOpened = (item: Item) =>
 export const isEmpty = (item: Item) => item.children.length === 0;
 
 export const needsToBeClosed = (item: Item) => item.isOpen;
+export const isRoot = (item: Item) => !item.parent;
 
 const getLastNestedItem = (item: Item): Item => {
   if (item.isOpen && item.children) {
@@ -51,15 +47,8 @@ const getLastNestedItem = (item: Item): Item => {
 };
 
 //this always returns following item without going down to children
-const getFollowingSibling = (item: Item): Item | undefined => {
-  const context = item.parent?.children;
-  if (context) {
-    const index = context.indexOf(item);
-    if (index < context.length - 1) {
-      return context[index + 1];
-    }
-  }
-};
+const getFollowingSibling = (item: Item): Item | undefined =>
+  getRelativeSibling(item, (currentIndex) => currentIndex + 1);
 
 const getFollowingItem = (item: Item): Item | undefined => {
   const followingItem = getFollowingSibling(item);
@@ -74,4 +63,16 @@ const getFollowingItem = (item: Item): Item | undefined => {
 };
 
 const isLast = (item: Item): boolean => !getFollowingSibling(item);
-export const isRoot = (item: Item) => !item.parent;
+
+const getRelativeSibling = (
+  item: Item,
+  getItemIndex: F2<number, number>
+): Item | undefined => {
+  const context = item.parent?.children;
+  if (context) {
+    const index = context.indexOf(item);
+    if (index < context.length - 1) {
+      return context[getItemIndex(index)];
+    }
+  }
+};
