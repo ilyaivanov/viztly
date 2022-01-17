8f8567cb64b48a7261298afc4cc1103058108011
Ilya Ivanov <static.ila@gmail.com>
2022-01-17 06:21:39 +0200

    PR fixes: remove braces from api/index.ts functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,18 +6,16 @@ const IS_USING_FAKE_API = true;
 export const loadItem = (item: Item) =>
   IS_USING_FAKE_API ? loadItemFaked(item) : loadItemReal(item);
 
-const loadItemReal = (item: Item): Promise<realAPI.MappedResponse> => {
-  return item.type === "YTplaylist"
+const loadItemReal = (item: Item): Promise<realAPI.MappedResponse> =>
+  item.type === "YTplaylist"
     ? realAPI.loadPlaylistItems(item)
     : item.type === "YTsearch"
     ? realAPI.loadSearchResults(item)
     : realAPI.loadChannelItems(item);
-};
 
-const loadItemFaked = (item: Item): Promise<realAPI.MappedResponse> => {
-  return item.type === "YTplaylist"
+const loadItemFaked = (item: Item): Promise<realAPI.MappedResponse> =>
+  item.type === "YTplaylist"
     ? fakeAPI.loadPlaylistItems(item)
     : item.type === "YTsearch"
     ? fakeAPI.loadSearchResults(item)
     : fakeAPI.loadChannelItems(item);
-};
